e786aa1b624cb4ae355e5acfaacea0a2f1fb9122
Shihab Uddin <contact.shihab.247@gmail.com>
2023-06-11 09:36:58 +0600

    sliders

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -10,68 +10,70 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Obx(
-            () => Positioned.fill(
-              child: ShaderView(
-                sizeOfCloud: controller.sizeOfCloud.value.toDouble(),
-                speedOfCloud: controller.speedOfCloud.value.toDouble(),
-              ),
-            ),
-          ),
-          Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(width: double.infinity),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: Image.asset(
-                  "assets/images/logo.jpg",
-                  height: 200,
-                  width: 200,
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Obx(
+              () => Positioned.fill(
+                child: ShaderView(
+                  sizeOfCloud: controller.sizeOfCloud.value.toDouble(),
+                  speedOfCloud: controller.speedOfCloud.value.toDouble(),
                 ),
               ),
-              const SizedBox(height: 20),
-              const Text("Trivia Quiz", style: TextStyle(fontSize: 30)),
-              const SizedBox(height: 50),
-              ElevatedButton(
-                onPressed: () => Get.toNamed(Routes.QUIZ),
-                child: const Text("Start Quiz"),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () => Get.toNamed(Routes.HISTORY),
-                child: const Text("History"),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () => Get.toNamed(Routes.SHADER),
-                child: const Text("Shader"),
-              ),
-              const SizedBox(height: 20),
-              Obx(
-                () => Slider(
-                    value: controller.sizeOfCloud.value.toDouble(),
-                    min: 100,
-                    max: 1000,
-                    onChanged: (value) {
-                      controller.sizeOfCloud.value = value.toInt();
-                    }),
-              ),
-              const SizedBox(height: 20),
-              Obx(
-                () => Slider(
-                    value: controller.speedOfCloud.value.toDouble(),
-                    onChanged: (value) {
-                      controller.speedOfCloud.value = value;
-                    }),
-              ),
-            ],
-          ),
-        ],
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(width: double.infinity),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image.asset(
+                    "assets/images/logo.jpg",
+                    height: 200,
+                    width: 200,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text("Trivia Quiz", style: TextStyle(fontSize: 30)),
+                const SizedBox(height: 50),
+                ElevatedButton(
+                  onPressed: () => Get.toNamed(Routes.QUIZ),
+                  child: const Text("Start Quiz"),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () => Get.toNamed(Routes.HISTORY),
+                  child: const Text("History"),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () => Get.toNamed(Routes.SHADER),
+                  child: const Text("Shader"),
+                ),
+                const SizedBox(height: 20),
+                Obx(
+                  () => Slider(
+                      value: controller.sizeOfCloud.value.toDouble(),
+                      min: 100,
+                      max: 1000,
+                      onChanged: (value) {
+                        controller.sizeOfCloud.value = value.toInt();
+                      }),
+                ),
+                const SizedBox(height: 20),
+                Obx(
+                  () => Slider(
+                      value: controller.speedOfCloud.value.toDouble(),
+                      onChanged: (value) {
+                        controller.speedOfCloud.value = value;
+                      }),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
